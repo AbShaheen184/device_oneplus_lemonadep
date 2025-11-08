3e65e285844dc7fff95cb702eebb2dd5e81c0873
AbShaheen184 <abuzar_shaheen2006@yahoo.com>
2025-11-08 18:59:05 +0300

    lemonadep: import lineage doze package

--- device.mk
+++ device.mk
@@ -36,6 +36,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946292801050754.xml
 
+# Oneplus Doze
+PRODUCT_PACKAGES += \
+    OplusDoze
+
 # Lineage Health
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/oplus_chg/battery/chg_enable)
 
